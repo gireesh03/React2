3ff24b2ada57eb9608ad7651b1612e40168ce594
gireesh03 <gireeshnani03@gmail.com>
2023-10-15 12:53:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Caluclator from "./Calculator.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Caluclator/>
+    </div>
+  );
+}
+
+export default App;
